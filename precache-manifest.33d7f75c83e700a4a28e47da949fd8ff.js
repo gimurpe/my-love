2c3190e64c70cc35c6ea4542c4e0188c65013e4e
Gilberto Murillo <gilbertomurillo@Gilbertos-MacBook-Pro.local>
2020-09-26 13:45:46 -0600

    Updates

---
+++ precache-manifest.33d7f75c83e700a4a28e47da949fd8ff.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b58ce8bc324f9c0fe1a41911b902e76",
+    "url": "/my-love/index.html"
+  },
+  {
+    "revision": "2105bb31ecd38b7f3a7d",
+    "url": "/my-love/static/css/main.865a188d.chunk.css"
+  },
+  {
+    "revision": "adcfb2b709c916002b67",
+    "url": "/my-love/static/js/2.b2635b10.chunk.js"
+  },
+  {
+    "revision": "4d6ab1395afd48c38ab429e9c82a1cee",
+    "url": "/my-love/static/js/2.b2635b10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2105bb31ecd38b7f3a7d",
+    "url": "/my-love/static/js/main.9ef80867.chunk.js"
+  },
+  {
+    "revision": "25aacaefad20169136d7",
+    "url": "/my-love/static/js/runtime-main.869e33df.js"
+  },
+  {
+    "revision": "84273157bed00cc8b895763ebef11d89",
+    "url": "/my-love/static/media/Poppins-Bold.84273157.ttf"
+  },
+  {
+    "revision": "a02d5c2b023be148d8d23d3b877d4745",
+    "url": "/my-love/static/media/Poppins-Bold.a02d5c2b.svg"
+  },
+  {
+    "revision": "bc1e47a3976358aa868a72de5a85de5a",
+    "url": "/my-love/static/media/Poppins-Bold.bc1e47a3.woff"
+  },
+  {
+    "revision": "e9f3ae9d44c53ce28c7ff2bc679d61f8",
+    "url": "/my-love/static/media/Poppins-Bold.e9f3ae9d.eot"
+  },
+  {
+    "revision": "83be73baef99815de859f45e997e7000",
+    "url": "/my-love/static/media/Poppins-BoldItalic.83be73ba.eot"
+  },
+  {
+    "revision": "89874573d9b2380672f3c5685263f6c4",
+    "url": "/my-love/static/media/Poppins-BoldItalic.89874573.woff"
+  },
+  {
+    "revision": "9f5c24370ace2d4f9f4e0e6d2e059603",
+    "url": "/my-love/static/media/Poppins-BoldItalic.9f5c2437.ttf"
+  },
+  {
+    "revision": "fabfc4152fe487c9d380365b4dc38a12",
+    "url": "/my-love/static/media/Poppins-BoldItalic.fabfc415.svg"
+  },
+  {
+    "revision": "2a47a29ceb33c966c8d79f8d5a5ea448",
+    "url": "/my-love/static/media/Poppins-Light.2a47a29c.ttf"
+  },
+  {
+    "revision": "46ba7d060101b6877c6674fe57bedae4",
+    "url": "/my-love/static/media/Poppins-Light.46ba7d06.svg"
+  },
+  {
+    "revision": "5c5aa25747e329a14d9ab8be881cbe02",
+    "url": "/my-love/static/media/Poppins-Light.5c5aa257.woff"
+  },
+  {
+    "revision": "dbe636b5970c150187ce640f9c1db216",
+    "url": "/my-love/static/media/Poppins-Light.dbe636b5.eot"
+  },
+  {
+    "revision": "0eb2e0a61fc86d1d9b41a5357b8f88fb",
+    "url": "/my-love/static/media/Poppins-Medium.0eb2e0a6.svg"
+  },
+  {
+    "revision": "1372de09cfc3e9a62af4234fd331e8e9",
+    "url": "/my-love/static/media/Poppins-Medium.1372de09.woff"
+  },
+  {
+    "revision": "597e1069f1687e85dda536d7eee9ea92",
+    "url": "/my-love/static/media/Poppins-Medium.597e1069.eot"
+  },
+  {
+    "revision": "ba95810b56f476990ca71d15139d5111",
+    "url": "/my-love/static/media/Poppins-Medium.ba95810b.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/my-love/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "46a7d48240d428c9dc3d4ff579199312",
+    "url": "/my-love/static/media/Poppins-Regular.46a7d482.woff"
+  },
+  {
+    "revision": "528cb4a87fbf5acd1aa772752b6848b1",
+    "url": "/my-love/static/media/Poppins-Regular.528cb4a8.eot"
+  },
+  {
+    "revision": "a2ed588ba986f48bab85d75d51fd80fb",
+    "url": "/my-love/static/media/Poppins-Regular.a2ed588b.svg"
+  },
+  {
+    "revision": "541e53bbd4a0b31334f423d9566fe3ad",
+    "url": "/my-love/static/media/Poppins-Thin.541e53bb.eot"
+  },
+  {
+    "revision": "ad37abfaab272d36dae659840954d1d6",
+    "url": "/my-love/static/media/Poppins-Thin.ad37abfa.woff"
+  },
+  {
+    "revision": "c0fafa8397437c95848724aed686d63b",
+    "url": "/my-love/static/media/Poppins-Thin.c0fafa83.ttf"
+  },
+  {
+    "revision": "f1cb351759d18f099c6a79b155aaa380",
+    "url": "/my-love/static/media/Poppins-Thin.f1cb3517.svg"
+  },
+  {
+    "revision": "352cf1ddd096799baa560159884fb515",
+    "url": "/my-love/static/media/Roboto-Bold.352cf1dd.eot"
+  },
+  {
+    "revision": "9c4bedeee9074a7ab438ff0e548d0fba",
+    "url": "/my-love/static/media/Roboto-Bold.9c4bedee.svg"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/my-love/static/media/Roboto-Bold.bb1e4dc6.woff"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/my-love/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "92f3b88eaefcf9e247e66f6ae41f53f6",
+    "url": "/my-love/static/media/ballon-couple.92f3b88e.gif"
+  },
+  {
+    "revision": "c2f8bdebe1afe2ecfbc9b46026f48d54",
+    "url": "/my-love/static/media/butterfly.c2f8bdeb.webm"
+  },
+  {
+    "revision": "b4525ec3c500950a039ed744df4c8a9f",
+    "url": "/my-love/static/media/heart-stamp.b4525ec3.png"
+  },
+  {
+    "revision": "4b33a72b5f6d6df7d00cc5dc770209da",
+    "url": "/my-love/static/media/stamp.4b33a72b.webp"
+  }
+]);
